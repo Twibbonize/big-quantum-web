989266028001218fa0f99f440a3c5b69165bbcbe
Aditya Pradana <adityabprada@gmail.com>
2023-12-05 17:12:27 +0700

    Add linear

---
+++ app/src/components/base/QSlider.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { onMounted } from 'vue';
+import { register } from 'swiper/element/bundle';
+
+const props = defineProps({
+  direction: String,
+  spaceBetween: Number,
+  grabCursor: Boolean,
+  speed: Number,
+  centeredSlides: Boolean,
+  loop: Boolean,
+  slidesPerView: String,
+  autoplayDelay: Number,
+  autoplayDisableOnInteraction: Boolean,
+  autoplayReverseDirection: Boolean,
+})
+
+onMounted(() => {
+  register();
+});
+</script>
+
+<template>
+  <swiper-container
+    style="--swiper-wrapper-transition-timing-function: linear;"
+    :direction="direction"
+    :space-between="spaceBetween"
+    :grab-cursor="grabCursor"
+    :speed="speed"
+    :centered-slides="centeredSlides"
+    :loop="loop"
+    :slides-per-view="slidesPerView"
+    :autoplay-delay="autoplayDelay"
+    :autoplay-disable-on-interaction="autoplayDisableOnInteraction"
+    :autoplay-reverse-direction="autoplayReverseDirection"
+  >
+    <slot></slot>
+  </swiper-container>
+</template>
+
+<style lang="scss">
+</style>
