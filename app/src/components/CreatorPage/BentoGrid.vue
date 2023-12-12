7788bf39123148aa5519417287821273d2d624b8
Aditya Pradana <adityabprada@gmail.com>
2023-12-12 12:43:48 +0700

    Fix bento grid gap

--- app/src/components/CreatorPage/BentoGrid.vue
+++ app/src/components/CreatorPage/BentoGrid.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="creators-features container px-5 py-24">
+    <div class="creators-features container px-5 py-24 flex flex-col gap-8">
         <div class="flex flex-col items-center">
             <h2 class="text-center text-3xl	sm:text-6xl font-bold">
                 Your all-in-one, user-friendly campaign platform
@@ -8,7 +8,7 @@
                 Tools to tailor and expand your campaign as your support Grows.
             </h4>
         </div>
-        <div class="gap-8 px-3 row mt-5">
+        <div class="flex flex-col sm:flex-row gap-8 px-3 row mt-5">
             <div class="card card-1 flex flex-col">
                 <h5 class="text-2xl sm:text-3xl font-bold">One of a king Twibbon Generator</h5>
                 <p class="text-sm sm:text-lg mt-4">
@@ -18,7 +18,7 @@
                 <a class="text-sm sm:text-lg font-bold text-black mt-6" href="#">Learn More</a>
             </div>
         </div>
-        <div class="gap-8 px-3 row mt-6">
+        <div class="flex flex-col sm:flex-row gap-8 px-3 row">
             <div class="card card-2 col">
                 <h5 class="text-2xl sm:text-3xl font-bold">See who supports you</h5>
                 <p class="text-sm sm:text-lg mt-4">
@@ -33,7 +33,7 @@
                 </p>
             </div>
         </div>
-        <div class="gap-8 px-3 row mt-6">
+        <div class="flex flex-col sm:flex-row gap-8 px-3 row">
             <div class="card card-4 col">
                 <h5 class="text-2xl sm:text-3xl font-bold">Know your supporters’ voices</h5>
                 <p class="text-sm sm:text-lg mt-4">
