41433d67a677b53350b0c6bf1bcb4aa6da53a37c
Aditya Pradana <adityabprada@gmail.com>
2023-12-14 18:17:46 +0700

    Add pills component

---
+++ app/src/components/atoms/QPills.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="pills rounded-4xl">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss">
+    .pills {
+      @apply bg-white;
+      border: 1px solid var(--color-light);
+      box-shadow: 0px 3.921px 23.523px 0px rgba(157, 170, 170, 0.12);
+    }
+</style>
