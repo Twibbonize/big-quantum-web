64856a84d48e4195a0e7771c40f411ef12e99ea8
Aditya Pradana <adityabprada@gmail.com>
2024-01-02 22:11:59 +0700

    Add bento for mobile view

--- app/src/components/CreatorPage/CreatorStories.vue
+++ app/src/components/CreatorPage/CreatorStories.vue
@@ -9,22 +9,24 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="creator-stories py-24 flex flex-col items-center">
+    <div class="creator-stories py-14 sm:py-24 flex flex-col items-center">
         <h4 class="text-center text-3xl sm:text-6xl font-bold">Stories from our Creators</h4>
-        <p class="text-center text-base sm:text-2xl mt-8">
+        <p class="text-center text-base sm:text-2xl mt-4 sm:mt-8">
             Unique campaigns brought to life with Twibbonize
         </p>
-        <div class="bento-container container w-full grid grid-cols-6 gap-6 px-5 mt-20">
-            <div class="bento-1 col-span-2 card p-5">
-                <h5 class="font-bold text-xl">Hanoi Art Book Fair Maximized Their Reach With Twibbonize</h5>
-                <p class="text-xs">Discover key challenges today’s marketing teams are facing, as well as opportunities for businesses in 2024</p>
-                <QOpenLink class="mt-6" classes="text-xs" text="Read Report" url="/"/>
+        <div class="bento-container container w-full grid grid-cols-1 sm:grid-cols-6 gap-6 sm:px-5 mt-6 sm:mt-20">
+            <div class="bento-1 sm:col-span-2 card">
+                <div class="pb-40 sm:pb-5 p-5">
+                    <h5 class="font-bold text-xl">Hanoi Art Book Fair Maximized Their Reach With Twibbonize</h5>
+                    <p class="text-xs">Discover key challenges today’s marketing teams are facing, as well as opportunities for businesses in 2024</p>
+                    <QOpenLink class="mt-6" classes="text-xs" text="Read Report" url="/"/>
+                </div>
                 <div class="phone-bg"></div>
                 <div class="green-bg"></div>
                 <div class="blue-bg"></div>
             </div>
-            <div class="bento-2 col-span-2 card">
-                <div class="p-5">
+            <div class="bento-2 sm:col-span-2 card">
+                <div class="pb-20 sm:pb-5 p-5">
                     <h5>“Twibbonize really helps us grow our audience with very little effort.”</h5>
                     <p class="font-bold text-xs mt-6">Clarissa May</p>
                     <p class="text-xs">Creative Director</p>
@@ -33,7 +35,7 @@ const props = defineProps({
                 <div class="girl-bg"></div>
                 <div class="yellow-bg"></div>
             </div>
-            <div class="bento-3 col-span-2 card">
+            <div class="bento-3 sm:col-span-2 card">
                 <img class="w-full" src="/assets/img/stories/stories-bento-3.png" alt="">
                 <div class="p-5">
                     <h5 class="font-bold text-xl">How Twibbonize Enhances Celebrations in Education</h5>
@@ -41,8 +43,8 @@ const props = defineProps({
                     <QOpenLink class="mt-6" classes="text-xs" text="Read Our Article" url="/"/>
                 </div>
             </div>
-            <div class="bento-4 card col-span-6 p-10">
-                <h5 class="text-4xl font-semibold w-1/2">Turn your creative ideas into actionable & marketable social campaigns</h5>
+            <div class="bento-4 card sm:col-span-6 p-5 sm:p-10">
+                <h5 class="text-xl sm:text-4xl font-semibold w-1/2">Turn your creative ideas into actionable & marketable social campaigns</h5>
                 <QButton variant="primary" class="mt-6">
                     Create Yours Now <i class="ri-arrow-right-line ml-1.5"></i>
                 </QButton>
@@ -164,10 +166,8 @@ const props = defineProps({
     }
 
     .bento-4 {
+        @apply bg-cover sm:bg-contain bg-left sm:bg-right bg-no-repeat;
         background-image: url('/assets/img/stories/stories-bento-4.png');
-        background-size: contain;
-        background-position: right center;
-        background-repeat: no-repeat;
     }
 }
 </style>
