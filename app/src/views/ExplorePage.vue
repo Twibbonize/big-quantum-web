5e6c67507773c9cca5b867bb41b4bb61b224837d
yogaaltariz <yoga.altariz@gmail.com>
2024-01-18 18:17:13 +0700

    change creator item in creators tab

--- app/src/views/ExplorePage.vue
+++ app/src/views/ExplorePage.vue
@@ -1,253 +1,224 @@
 <script setup>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue';
+import { Listbox, ListboxButton, ListboxOptions, ListboxOption } from '@headlessui/vue';
+import LayoutMain from '@/components/layouts/LayoutMain.vue';
+import QListbox from '@/components/atoms/forms/QListbox.vue';
+import QInputText from '@/components/atoms/forms/QInputText.vue';
+import CampaignCard from '@/components/molecules/CampaignCard.vue';
+import CollectionCard from '@/components/molecules/CollectionCard.vue';
+import CreatorCard from '@/components/molecules/CreatorCard.vue';
+import CreatorBox from '@/components/elements/CreatorBox.vue';
+
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 
-import LayoutMain from '@/components/layouts/LayoutMain.vue';
-import CampaignCard from '@/components/molecules/CampaignCard.vue';
-import CollectionCard from '@/components/molecules/CollectionCard.vue';
+import { publicCampaigns } from '@/mock/campaigns';
+import { publicCollections } from '@/mock/collections';
+import { creators } from '@/mock/creators';
 
-import { getAvatarUrl, getThumbnailUrl } from '@/utils/urls.js';
-
-const campaigns = computed(() => {
-    const samples = [
-        {
-            name: 'UNIVERSE UNPACKED 2022',
-            creator: {
-                name: 'Universe Tech',
-                avatar: 'sample-avatar-1.jpg'
-            },
-            hit: 85500,
-            createdAt: dayjs().subtract(5, 'day').fromNow(),
-            thumbnail: 'sample-campaign-1.jpg'
-        },
-        {
-            name: 'Universe Tech Future Developers Bootcamp',
-            creator: {
-                name: 'Universe Tech',
-                avatar: 'sample-avatar-1.jpg'
-            },
-            hit: 1470,
-            createdAt: dayjs().subtract(3, 'week').fromNow(),
-            thumbnail: 'future-developers.jpg'
-        },
-        {
-            name: 'Universe Tech Innovation Day',
-            creator: {
-                name: 'Universe Tech',
-                avatar: 'sample-avatar-1.jpg'
-            },
-            hit: 15100,
-            createdAt: dayjs().subtract(5, 'day').fromNow(),
-            thumbnail: 'innovation-day-1.jpg'
-        },
-        {
-            name: 'Shot on Universe Phone',
-            creator: {
-                name: 'Universe Tech',
-                avatar: 'sample-avatar-1.jpg'
-            },
-            hit: 7700,
-            createdAt: dayjs().subtract(1, 'week').fromNow(),
-            thumbnail: 'shot-on.jpg'
-        }
-    ];
-
-    return samples.map(({ thumbnail, creator, ...other }) => {
-        const { name, avatar } = creator;
-        return {
-            ...other,
-            thumbnail: getThumbnailUrl(thumbnail),
-            creator: {
-                name,
-                avatar: getAvatarUrl(avatar)
-            }
-        };
-    });
+const props = defineProps({
+    tab: {
+        type: String,
+        default: 'campaign'
+    }
 });
 
-const collections = [
-    {
-        name: '🥳 Happy Birthday!',
-        campaigns: [
-            {
-                name: 'Purple and White Modern Happy Birthday',
-                thumbnail: '/assets/img/campaigns/campaign-hbd-1.jpg'
-            },
-            {
-                name: 'Blue White Happy Birthday',
-                thumbnail: '/assets/img/campaigns/campaign-hbd-2.jpg'
-            },
-            {
-                name: 'Pink Colourful Happy Birthday',
-                thumbnail: '/assets/img/campaigns/campaign-hbd-3.jpg'
-            },
-            {
-                name: 'Water Colour Outer Space Happy Birthday',
-                thumbnail: '/assets/img/campaigns/campaign-hbd-4.webp'
-            }
-        ],
-        uri: 'happy-birthday',
-        creator: {
-            name: 'Universe Tech',
-            avatar: '/assets/img/sample/sample-avatar-1.jpg',
-            username: 'universetech'
-        }
-    },
-    {
-        name: '👶 New Baby',
-        campaigns: [
-            {
-                name: 'Neutral New Baby Born',
-                thumbnail: '/assets/img/campaigns/campaign-new-baby-1.jpg'
-            },
-            {
-                name: 'Beige White New Baby Born',
-                thumbnail: '/assets/img/campaigns/campaign-new-baby-2.jpg'
-            },
-            {
-                name: 'Pink White Baby Birth',
-                thumbnail: '/assets/img/campaigns/campaign-new-baby-3.jpg'
-            },
-            {
-                name: 'Pink and Orange Playful Little Family Twibbon',
-                thumbnail: '/assets/img/campaigns/campaign-new-baby-4.webp'
-            }
-        ],
-        uri: 'new-baby',
-        creator: {
-            name: 'Universe Tech',
-            avatar: '/assets/img/sample/sample-avatar-1.jpg',
-            username: 'universetech'
-        }
-    },
-    {
-        name: '🎓 Graduation',
-        campaigns: [
-            {
-                name: 'Brown and Cream Happy Graduation',
-                thumbnail: '/assets/img/campaigns/campaign-graduation-1.jpg'
-            },
-            {
-                name: 'Pink Cute Happy Graduation',
-                thumbnail: '/assets/img/campaigns/campaign-graduation-2.jpg'
-            },
-            {
-                name: 'Yellow and Green Happy Graduation',
-                thumbnail: '/assets/img/campaigns/campaign-graduation-3.jpg'
-            },
-            {
-                name: 'Blue and Yellow Modern Happy Graduation',
-                thumbnail: '/assets/img/campaigns/campaign-graduation-4.webp'
-            }
-        ],
-        uri: 'graduation',
-        creator: {
-            name: 'Universe Tech',
-            avatar: '/assets/img/sample/sample-avatar-1.jpg',
-            username: 'universetech'
-        }
-    },
-    {
-        name: '💍 Wedding Celebration',
-        campaigns: [
-            {
-                name: 'Brown and Cream Happy Graduation',
-                thumbnail: '/assets/img/campaigns/campaign-wedding-celebration-1.jpg'
-            }
-        ],
-        uri: 'graduation',
-        creator: {
-            name: 'Universe Tech',
-            avatar: '/assets/img/sample/sample-avatar-1.jpg',
-            username: 'universetech'
-        }
-    }
-];
+const router = useRouter();
+const tabs = ['campaign', 'collection', 'creator'];
+
+const sortCampaignOptions = [{ name: 'Trending' }, { name: 'Most Popular' }];
+const selectedSortCampaign = ref(sortCampaignOptions[0]);
+
+const onChangeTab = (index) => {
+    const nextTab = tabs[index];
+    router.push({ name: 'explore', params: { tab: nextTab } });
+};
+
+const selectedTabIndex = computed(() => {
+    const paramTab = props.tab;
+    return tabs.findIndex((item) => paramTab === item);
+});
 </script>
+
 <template>
-    <LayoutMain>
-        <div class="page mt-8">
-            <div class="container">
-                <div class="space-y-24">
-                    <div class="space-y-10">
-                        <div class="flex items-end justify-between">
-                            <div class="space-y-3">
-                                <div class="text-sm">Campaign</div>
-                                <div class="text-4xl font-bold max-w-lg">
-                                    Latest trending in your country
-                                </div>
-                            </div>
-                            <p>
-                                Don't miss the latest happenings in
-                                <span class="font-semibold text-black">Indonesia</span>.
-                            </p>
-                        </div>
+    <LayoutMain navbarColor="white" :navbarShadow="false">
+        <div class="page explore">
+            <TabGroup
+                :selectedIndex="selectedTabIndex"
+                as="div"
+                class="explore__tab-group"
+                @change="onChangeTab"
+            >
+                <div class="explore__wrapper explore__tabs-wrapper border-t border-stroke">
+                    <div class="container px-4 xl:px-0">
+                        <div class="flex items-center justify-between">
+                            <TabList class="explore__tab-list w-full md:w-auto">
+                                <Tab v-slot="{ selected }" as="template">
+                                    <button
+                                        :class="[
+                                            'explore__tab',
+                                            selected && 'explore__tab--selected'
+                                        ]"
+                                    >
+                                        <i class="ri-megaphone-line"></i>
+                                        <span class="ml-1.5 lg:flex-grow-1">Campaigns</span>
+                                    </button>
+                                </Tab>
 
-                        <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
-                            <campaign-card v-for="campaign in campaigns" v-bind="campaign" />
-                        </div>
+                                <Tab v-slot="{ selected }" as="template">
+                                    <button
+                                        :class="[
+                                            'explore__tab',
+                                            selected && 'explore__tab--selected'
+                                        ]"
+                                    >
+                                        <i class="ri-folder-image-line"></i>
+                                        <span class="ml-1.5 lg:flex-grow-1"> Collections </span>
+                                    </button>
+                                </Tab>
 
-                        <div class="flex items-center justify-center space-x-2">
-                            <div class="text-content">Discover a huge of inspiring campaigns</div>
-                            <button class="space-x-1 font-semibold">
-                                <i class="ri-arrow-right-line"></i>
-                                <span>View More Campaigns</span>
-                            </button>
-                        </div>
-                    </div>
+                                <Tab v-slot="{ selected }" as="template">
+                                    <button
+                                        :class="[
+                                            'explore__tab',
+                                            selected && 'explore__tab--selected'
+                                        ]"
+                                    >
+                                        <i class="ri-user-line"></i>
+                                        <span class="ml-1.5 lg:flex-grow-1"> Creators </span>
+                                    </button>
+                                </Tab>
+                            </TabList>
 
-                    <div class="space-y-10">
-                        <div class="flex items-end justify-between">
-                            <div class="space-y-3">
-                                <div class="text-sm">Creators</div>
-                                <div class="text-4xl font-bold max-w-lg">Top Creators</div>
-                            </div>
-                        </div>
-
-                        <div class="flex items-center justify-center space-x-2">
-                            <div class="text-content">Explore with over 5,191 Creators</div>
-                            <button class="space-x-1 font-semibold">
-                                <i class="ri-arrow-right-line"></i>
-                                <span>View More Creators</span>
-                            </button>
-                        </div>
-                    </div>
-
-                    <div class="space-y-10">
-                        <div class="flex items-end justify-between">
-                            <div class="space-y-3">
-                                <div class="text-sm">Collections</div>
-                                <div class="text-4xl font-bold max-w-lg">
-                                    Explore a wide variety of collections.
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="grid grid-cols-1 sm:grid-cols-2` lg:grid-cols-4 gap-6">
-                            <RouterLink
-                                v-for="collection in collections"
-                                :key="collection.uri"
-                                :to="{
-                                    name: 'collection',
-                                    params: { uri: collection.uri }
-                                }"
+                            <div
+                                v-show="selectedTabIndex === 0"
+                                class="max-w-xs flex-grow hidden md:block"
                             >
-                                <CollectionCard v-bind="collection" />
-                            </RouterLink>
-                        </div>
-
-                        <div class="flex items-center justify-center space-x-2">
-                            <div class="text-content">Browse specially curated campaigns</div>
-                            <button class="space-x-1 font-semibold">
-                                <i class="ri-arrow-right-line"></i>
-                                <span>View More Collections</span>
-                            </button>
+                                <QListbox
+                                    v-model="selectedSortCampaign"
+                                    :options="sortCampaignOptions"
+                                    name="campaigns_sort"
+                                    prefix="Sort by: "
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+
+                <TabPanels>
+                    <TabPanel>
+                        <Transition name="fade" mode="out-in">
+                            <div class="explore__tab-panel">
+                                <div class="container px-4 xl:px-0 py-6">
+                                    <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
+                                        <CampaignCard
+                                            v-for="campaign in publicCampaigns"
+                                            v-bind="campaign"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </Transition>
+                    </TabPanel>
+
+                    <TabPanel>
+                        <Transition name="fade" mode="out-in">
+                            <div class="explore__tab-panel">
+                                <div class="container px-4 xl:px-0 py-6">
+                                    <div
+                                        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6"
+                                    >
+                                        <RouterLink
+                                            v-for="collection in publicCollections"
+                                            :key="collection.uri"
+                                            :to="{
+                                                name: 'collection',
+                                                params: { uri: collection.uri }
+                                            }"
+                                        >
+                                            <CollectionCard v-bind="collection" />
+                                        </RouterLink>
+                                    </div>
+                                </div>
+                            </div>
+                        </Transition>
+                    </TabPanel>
+
+                    <TabPanel>
+                        <Transition name="fade" mode="out-in">
+                            <div class="explore__tab-panel">
+                                <div class="container px-4 xl:px-0 py-6">
+                                    <div
+                                        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-6 md:gap-y-10 gap-x-6"
+                                    >
+                                        <CreatorBox
+                                            v-for="creator in creators"
+                                            v-bind="creator"
+                                            :bordered="false"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </Transition>
+                    </TabPanel>
+                </TabPanels>
+            </TabGroup>
         </div>
     </LayoutMain>
 </template>
+
+<style scoped lang="scss">
+.explore {
+    .explore__wrapper {
+        @apply border-b border-stroke;
+    }
+
+    .explore__tabs-wrapper {
+        @apply sticky bg-white z-40;
+        top: 76px;
+
+        @include md_screen {
+            top: 88px;
+        }
+    }
+
+    .explore__tab-list {
+        @apply flex items-center justify-between space-x-2 md:space-x-6;
+    }
+
+    .explore__tab {
+        @apply relative outline-none flex-shrink-0 py-4 md:py-6 flex items-center justify-center h-full text-black/50 tracking-wide font-medium cursor-pointer transition-colors duration-200 ease-out flex-grow;
+
+        font-size: 0.75rem;
+
+        @include sm {
+            @apply flex-grow;
+        }
+
+        @include md_screen {
+            font-size: 0.9rem;
+        }
+
+        &:hover,
+        &--selected {
+            @apply text-black;
+        }
+
+        &--selected::after {
+            width: 100%;
+            height: 2px;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            content: '';
+            display: block;
+            background-color: black;
+            border-top-right-radius: 0.5px;
+            border-top-left-radius: 0.5px;
+        }
+    }
+}
+</style>
