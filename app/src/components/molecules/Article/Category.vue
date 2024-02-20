dea9981bd26f862fd9c13d3af03304749ad2977d
Aditya Pradana <adityabprada@gmail.com>
2024-02-20 18:22:49 +0700

    Add component on articcle page

---
+++ app/src/components/molecules/Article/Category.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+    }
+});
+
+const categoryColor = computed(() => {
+    if (/(events)/ig.test(props.title)) return '--category-primary: #4D41B9; --category-secondary: #F9F5FF;';
+    return '--category-primary: #4D41B9; --category-secondary: #F9F5FF;';
+});
+</script>
+
+<template>
+    <div class="article-category" :style="categoryColor">
+        <h4>{{ title }}</h4>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.article-category {
+    @apply py-1 px-3 rounded-2xl w-max;
+    background: var(--category-secondary);
+
+    h4 {
+        @apply text-[#667085] sm:text-base font-medium uppercase;
+        color:  var(--category-primary);
+        letter-spacing: 2.5px;
+    }
+}
+</style>
---
+++ app/src/views/ArticlePage.vue
@@ -0,0 +1,67 @@
+<script setup>
+import LayoutMain from '@/components/layouts/LayoutMain.vue';
+import QBreadcrumbs from '@/components/atoms/QBreadcrumbs.vue';
+import ArticleMore from '@/components/molecules/Article/More.vue';
+import ArticleCategory from '@/components/molecules/Article/Category.vue';
+
+const links = [
+    {
+        title: 'Use Case',
+        link: '/use-cases',
+    },
+    {
+        title: 'How Twibbonize Becomes a Visual Marvel for Various Events',
+    },
+];
+
+const moreLinks = [
+    {
+        title: 'How Twibbonize Innovates Government Communication in the Digital Age',
+        link: '/use-cases',
+    },
+    {
+        title: 'Twibbonize Elevate Personal Celebrations with Customized Digital Flair',
+        link: '/use-cases',
+    },
+    {
+        title: 'How Twibbonize Elevates Political Engagement with Visual Frames',
+        link: '/use-cases',
+    },
+    {
+        title: 'Empowering Social Causes Through Twibbonize\'s Visual Impact',
+        link: '/use-cases',
+    },
+]
+</script>
+
+<template>
+    <LayoutMain>
+        <div class="article-page">
+            <QBreadcrumbs :links="links"/>
+            <div class="article-container">
+                <div class="article-content">
+                    <ArticleCategory title="events"/>
+                </div>
+                <ArticleMore  class="article-more" title="More Use Cases" :links="moreLinks"/>
+            </div>
+        </div>
+    </LayoutMain>
+</template>
+
+<style lang="scss" scoped>
+.article-page {
+    @apply container sm:mt-[88px] px-5 sm:py-14 flex flex-col gap-16;
+
+    .article-container {
+        @apply w-full flex gap-[30px];
+    }
+
+    .article-content {
+        @apply sm:max-w-[820px] w-full;
+    }
+
+    .article-more {
+        @apply sm:max-w-[256px];
+    }
+}
+</style>
