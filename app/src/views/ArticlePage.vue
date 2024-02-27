dc387f3dade10a77fc4c1b2a1b83b2280e7892b0
Aditya Pradana <adityabprada@gmail.com>
2024-02-27 11:40:23 +0700

    Add title and description

--- app/src/views/ArticlePage.vue
+++ app/src/views/ArticlePage.vue
@@ -4,6 +4,7 @@ import QBreadcrumbs from '@/components/atoms/QBreadcrumbs.vue';
 import ArticleMore from '@/components/molecules/Article/More.vue';
 import ArticleCategory from '@/components/molecules/Article/Category.vue';
 import ArticleTitle from '@/components/molecules/Article/Title.vue';
+import ArticleBlock from '@/components/molecules/Article/Block.vue';
 
 const links = [
     {
@@ -40,13 +41,19 @@ const articleData = {
     readtime: 4,
     content: [
         {
-
+            paragraph: 'In an era where digital engagement plays a pivotal role in shaping event experiences, Twibbonize emerges as a transformative ally for organizers seeking innovative ways to promote and elevate their events. This dynamic digital tool goes beyond conventional promotional strategies, offering organizers a multifaceted approach to prepare their digital marketing assets, engage with participants, and amplify their event visibility. See how this versatile tool enriches the landscape of event promotion by providing multifaceted solutions for event organizers.',
+            line: true,
         },
         {
-
+            subtitle: 'Facilitating Marketing Asset Creation',
+            paragraph: 'Twibbonize functions as a valuable tool for organizers to seamlessly create marketing assets. Usually, Organizers need a unified design for social media posts in large quantities produced within a short period. There is this unspoken demand about making social media channels, usually Instagram, aesthetically pleasing and emphasize visual appearance. This functionality streamlines the process of generating visually cohesive and engaging promotional material. By providing organizers with the ability to quickly craft eye-catching frames that align with their event\'s branding, Twibbonize becomes an essential asset for producing marketing collateral that resonates with the target audience. This feature enhances efficiency, empowering organizers to work more effectively in creating compelling visual content for their promotional campaigns.'
         },
         {
-
+            subtitle: 'Enhancing Digital Marketing and Online Presence',
+            paragraph: 'The adoption of event-specific frames on social media platforms becomes a pivotal element in the digital marketing strategy for Organizers to promote their events. It serves as a visually compelling call-to-action, prompting the general public and their target market to engage with the event tangibly. When these twibbon frames circulate on participants\' profiles or exist in their social media posts, participants have already contributed to the event\'s online presence, creating a buzz that extends beyond traditional promotional methods. This user-driven content becomes a valuable asset for digital marketing campaigns, providing authentic and visually appealing material to showcase the event across various digital channels that benefit the Organizers and the event.'
+        },
+        {
+            paragraph: 'Organizers from different industries can adapt and implement these functions at various events across music, fashion, conventions, awards, film, food and beverages, bazaars, workshops, sports, even competitions. We try to collect various examples from real creators on our platform that utilize Twibbonize as an extension of their Marketing measure by utilizing twibbon frames that you can learn from.'
         }
     ],
 }
@@ -57,16 +64,19 @@ const articleData = {
         <div class="article-page">
             <QBreadcrumbs :links="links"/>
             <div class="article-container">
-                <div class="article-content">
+                <article class="article-content">
                     <ArticleCategory title="events"/>
                     <ArticleTitle
                         :title="articleData.title"
                         :published-date="articleData.publishedDate"
                         :readtime="articleData.readtime"
                     />
-                    <ArticleBlock/>
-                </div>
-                <ArticleMore  class="article-more" title="More Use Cases" :links="moreLinks"/>
+                    <ArticleBlock
+                        v-for="block in articleData.content"
+                        :block="block"
+                    />
+                </article>
+                <ArticleMore class="article-more" title="More Use Cases" :links="moreLinks"/>
             </div>
         </div>
     </LayoutMain>
