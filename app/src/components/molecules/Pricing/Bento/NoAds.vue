a61c0c7ac6627cc2f486195e4556f28043dad97b
Aditya Pradana <adityabprada@gmail.com>
2024-03-06 00:06:39 +0700

    Fix pricing style

---
+++ app/src/components/molecules/Pricing/Bento/NoAds.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex">
+        <div class="flex flex-col justify-between">
+            <video
+                class="w-full"
+                autoplay loop muted
+                src="/assets/videos/pricing-no-ads.mp4"
+            ></video>
+            <div class="p-4">
+                <p class="text-center text-xl sm:text-2xl">Use Twibbonize</p>
+                <p class="text-center text-xl sm:text-2xl"><strong>without Ads</strong></p>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ app/src/components/molecules/Pricing/Bento/NoExtraFeatures.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex flex-col items-center justify-center h-[320px] p-4">
+        <p class="text-7xl">😔</p>
+        <p class="text-center font-bold text-xl sm:text-2xl">No Extra Features</p>
+        <p class="text-center text-xl sm:text-2xl">Available</p>
+    </div>
+</template>
